8416c5b1882a8254d7a5422215a1fe75b7252713
youmichoi93 <78770263+youmichoi93@users.noreply.github.com>
2021-03-09 17:33:51 +0900

    Add files via upload

---
+++ lecture12.js
@@ -0,0 +1,48 @@
+//consol
+
+
+var str = "Hello"
+undefined
+str.length;
+5
+var str2 = "World";
+undefined
+str.concat(str2);
+"HelloWorld"
+str.concat(str2).concat("!")
+    
+"HelloWorld!"
+"Hello".concat("WOrld").concat("!");
+"HelloWOrld!"
+"hello".lenght
+undefined
+"hello".lenght
+undefined
+str + str2;
+"HelloWorld"
+"Hello" + "World"
+"HelloWorld"
+var str = "abcdeabcde";
+undefined
+str.charAt(0);
+"a"
+str.lengh
+undefined
+"Hello".lenght
+undefined
+str.lenght
+undefined
+str.lengh
+undefined
+str.length
+10
+str = "abcdeabcde"
+"abcdeabcde"
+str.charAt(str.lenght - 5);
+"a"
+str.substring(2,4)
+"cd"
+str.substr(2,4);
+"cdea"
+str.substr(-7,2);
+"de"
